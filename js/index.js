aedec672ee792e24db07dc01e11e696846a8a149
Tech-A10 <tech-a10@MacBook-Air-8.local>
2018-02-19 23:20:42 +0000

    fixed a few bits + working bonus code

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 
+var user_selection = prompt("What Calculator mode do you want to use? Basic, Advanced, BMI or Trip Calculator?");
 
-var user_selection = prompt("What Calculator mode do you want to use? Basic, Advanced, BMI (Imperial) or BMI (Metric)?");
+// Prompt Menu
 
 if (user_selection == "Basic"){
 
@@ -15,15 +16,16 @@ if (user_selection == "Basic"){
 	var num2 = parseInt(prompt("Enter your second number"));
 	var power_operator = prompt("Which Advanced function would you like to use? (a)Power (b)Square Root");
 
-} else if (user_selection == 'BMI (Imperial)'){
-	var height=prompt("Enter your height in inches.");
-	var weight=prompt("Enter your weight in pounds");
-	var BMI=(weight/(height*height))*703;
-} else if (user_selection == 'BMI (Metric)'){
-	var height_metric=prompt("Enter your height in Meters.");
-	var weight_metric=prompt("Enter your weight in Kg");
-	var BMI_metric=(weight/(height*height))*10000;
+} else if (user_selection == 'BMI'){
+	var bmi_operator = prompt("Which Units would you like to use? (a)Metric, (b)Imperial");
+
+}else{
+	alert("Invalid Option. Please try again by refereshing the page!");
 }
+
+
+
+//  Operators
 
 if (operator_selector == '+'){
 	alert(num1+num2);
@@ -35,30 +37,26 @@ if (operator_selector == '+'){
 	alert(num1/num2);
 } 
 
+// Powers
+
 if (power_operator =='a'){
 	alert(num1 + " to the power of " + num2 + " is " +Math.pow(num1,num2));
 } else if (power_operator == 'b'){
 	alert("The Square Roots of both numbers are" +Math.sqrt(num1)+ "and" +Math.sqrt(num2));
 }
 
-if(BMI<=18.5){
-	status='Underweight';
-}else if (BMI<=24.9){
-	status='Normal';
-}else if (BMI<=29.9){
-	status='Overweight';
-}else{
-	status='Obese';
-}alert(status);
 
-if(BMI_metric<=18.5){
-	status='Underweight';
-}else if (BMI_metric<=24.9){
-	status='Normal';
-}else if (BMI_metric<=29.9){
-	status='Overweight';
-}else{
-	status='Obese';
-}alert(status);
+// Bonus 
+
+if (bmi_operator == "a"){
+	var height=parseInt(prompt("Enter your height in inches."));
+	var weight=parseInt(prompt("Enter your weight in pounds"));
+	alert("Your BMI Is" + (703*weight)/(Math.pow(height,2)));
+} else if (bmi_operator == "b"){
+	var heightmetric=parseInt(prompt("Enter your height in Meters."));
+	var weightmetric=parseInt(prompt("Enter your weight in Kg"));
+	alert("Your BMI Is" + (10000*weightmetric)/(Math.pow(height,2)));
+} 
+
 
 
